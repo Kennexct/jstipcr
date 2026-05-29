19436e607961c89e5c03755de1c2b95f0e516734
Kennexct <kenny.chen25@gmail.com>
2026-05-29 15:46:09 +0700

    fix error upload fix bottleneck

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(() => {
         output: {
           manualChunks: {
             vendor: ['react', 'react-dom', 'react-router-dom'],
-            ui: ['lucide-react', 'recharts', 'motion', 'shadcn'],
+            ui: ['lucide-react', 'recharts', 'motion'],
             utils: ['date-fns', 'clsx', 'tailwind-merge']
           }
         }
